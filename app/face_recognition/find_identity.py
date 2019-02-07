fd8cad3e2d9bae33571d720e22cb9b72e8b04bce
whiteRa2bit <pavel.fakanov@gmail.com>
2019-02-07 11:00:44 +0300

    small changes

--- app/face_recognition/find_identity.py
+++ app/face_recognition/find_identity.py
@@ -11,17 +11,17 @@ def get_identity(face_id):
     face_input = Face.objects.filter(id=face_id).first()
     faces_query = Face.objects.filter(avatar__isnull=False)
 
-    embedding_input = face_input['embedding']
+    embedding_input = face_input.embedding
     embedding_input = np.array(embedding_input)
     avatar_id = -1
     avatar_dist = float('Inf')
 
     for face in faces_query:
-        next_embedding = face['embedding']
+        next_embedding = face.embedding
         next_embedding = np.array(next_embedding)
         norm = np.linalg.norm(embedding_input - next_embedding)
         if norm < avatar_dist:
-            avatar_id = face['avatar']
+            avatar_id = face.avatar.id
             avatar_dist = norm
 
     if avatar_dist < threshold:
