89521a4ac6e829342c322dffca2f987afb96afdf
whiteRa2bit <pavel.fakanov@gmail.com>
2019-02-08 10:18:20 +0300

    change register page

--- UI/src/components/register.vue
+++ UI/src/components/register.vue
@@ -3,25 +3,68 @@
         <h1>Регистрация</h1>
         <div class="inputFields">
             <span v-if="empty" class="error">*Все поля должны быть заполнены.</span>
+            <span v-if="!passwords_are_equal" class="error">*Введенные пароли должны совпадать.</span>
             <span v-if="incorrect" class="error">*Такой пользователь уже существует.</span>
             <div class="user-input-wrp">
                 <br/>
-                <input type="text" class="input" id="loginField" name="username" v.model="input.username" v-on:change="updateUsername" autofocus required align="center" tabindex="1" />
+                <input
+                        type="text"
+                        class="input"
+                        id="loginField"
+                        name="username"
+                        reg="username"
+                        v.model="input.username"
+                        v-on:change="updateUsername"
+                        v-on:keyup.enter="register"
+                        autofocus required
+                        align="center"
+                        tabindex="1" />
                 <span class="floating-label">Логин</span>
             </div>
             <div class="user-input-wrp">
                 <br/>
-                <input type="password" class="input" id="passwordField" name="password" v.model="input.password" v-on:change="updatePassword" required tabindex="2" />
+                <input
+                        type="password"
+                        class="input"
+                        id="passwordField"
+                        name="password"
+                        ref="password"
+                        v.model="input.password"
+                        v-on:change="updatePassword"
+                        v-on:keyup.enter="register"
+                        required tabindex="2" />
                 <span class="floating-label">Пароль</span>
             </div>
             <div class="user-input-wrp">
                 <br/>
-                <input type="text" class="input" id="emailField" name="email" v.model="input.email" v-on:change="updateEmail" required tabindex="3" />
+                <input
+                        type="password"
+                        class="input" id="confirmPasswordField"
+                        name="confirm_password"
+                        ref="confirm_password"
+                        v.model="input.confirm_password"
+                        v-on:change="updateConfirmPassword"
+                        v-on:keyup.enter="register"
+                        required tabindex="3" />
+                <span class="floating-label">Подтвердите пароль</span>
+            </div>
+            <div class="user-input-wrp">
+                <br/>
+                <input
+                        type="text"
+                        class="input"
+                        id="emailField"
+                        name="email"
+                        ref="email"
+                        v.model="input.email"
+                        v-on:change="updateEmail"
+                        v-on:keyup.enter="register"
+                        required tabindex="4" />
                 <span class="floating-label">Электронная почта</span>
             </div>
-            <button class="button" id="registerButton" form="register" type="button" v-on:click="register()" tabindex="4"><span>Регистрация</span></button>
+            <button class="button" id="registerButton" form="register" type="button" v-on:click="register()" tabindex="5"><span>Регистрация</span></button>
         </div>
-        <button class="button" id="loginButton" form="register" type="button" v-on:click="toLoginPage()" tabindex="5">Уже есть аккаунт? Войти.</button>
+        <button class="button" id="loginButton" form="register" type="button" v-on:click="toLoginPage()" tabindex="6">Уже есть аккаунт? Войти.</button>
     </form>
 </template>
 
@@ -34,29 +77,49 @@
                 empty: true,
                 correct: true,
                 incorrect: false,
+                passwords_are_equal: true,
                 input: {
                     username: "",
                     password: "",
+                    confirm_password: "",
                     email: ""
                 }
             }
         },
         methods: {
             register() {
-                var this_ = this;
-                if(!this.empty) {
-                    axios.post('/api/sign_up/', {username: this.input.username, password: this.input.password, email: this.input.email}).then(function (response) {
-                        localStorage.token = response.data.token;
-                        this_.$emit("authenticated", true);
-                        this_.$router.replace({ name: "secure" });
-                    }).catch(function (error) {
-                        console.log(error);
-                        this_.incorrect = true;
-                    })
-                    /*localStorage.token = 228;
-                    this.$emit("authenticated", true);
-                    this.$router.replace({ name: "secure" });*/
+                if (!(this.input.username != "" && this.input.password != "" &&
+                    this.input.confirm_password && this.input.email != "")) {
+                    if (!this.input.username) {
+                        this.$refs.username.focus()
+                    } else if (!this.input.password) {
+                        this.$refs.password.focus()
+                    } else if (!this.input.confirm_password) {
+                        this.$refs.confirm_password.focus()
+                    } else if (!this.input.emal) {
+                        this.$refs.email.focus()
+                    }
+                } else {
+                    var this_ = this;
+                    if(!this.empty) {
+                        if (this.input.password != this.input.confirm_password) {
+                            this.passwords_are_equal = false;
+                        } else {
+                            axios.post('/api/sign_up/', {username: this.input.username, password: this.input.password, email: this.input.email}).then(function (response) {
+                                localStorage.token = response.data.token;
+                                this_.$emit("authenticated", true);
+                                this_.$router.replace({ name: "secure" });
+                            }).catch(function (error) {
+                                console.log(error);
+                                this_.incorrect = true;
+                            })
+                            /*localStorage.token = 228;
+                            this.$emit("authenticated", true);
+                            this.$router.replace({ name: "secure" });*/
+                        }
+                    }
                 }
+
             },
             toLoginPage() {
                 this.$router.replace({ name: "login" });
@@ -64,7 +127,8 @@
             updateUsername(event) {
                 this.input.username = event.target.value;
                 this.incorrect = false;
-                if (!(this.input.username != "" && this.input.password != "" && this.input.email != "")) {
+                if (!(this.input.username != "" && this.input.password != "" &&
+                                                    this.input.confirm_password && this.input.email != "")) {
                     this.empty = true;
                 }
                 else {
@@ -74,17 +138,44 @@
             updatePassword(event) {
                 this.input.password = event.target.value;
                 this.incorrect = false;
-                if (!(this.input.username != "" && this.input.password != "" && this.input.email != "")) {
+                if (!(this.input.username != "" && this.input.password != "" &&
+                                                    this.input.confirm_password && this.input.email != "")) {
                     this.empty = true;
                 }
                 else {
                     this.empty = false;
                 }
+
+                if (this.input.password != this.input.confirm_password && this.input.password
+                                                                        && this.input.confirm_password) {
+                    this.passwords_are_equal = false;
+                } else {
+                    this.passwords_are_equal = true;
+                }
+            },
+            updateConfirmPassword(event) {
+                this.input.confirm_password = event.target.value;
+                this.incorrect = false;
+                if (!(this.input.username != "" && this.input.password != "" &&
+                                                    this.input.confirm_password && this.input.email != "")) {
+                    this.empty = true;
+                }
+                else {
+                    this.empty = false;
+                }
+
+                if (this.input.password != this.input.confirm_password && this.input.password
+                                                                        && this.input.confirm_password) {
+                    this.passwords_are_equal = false;
+                } else {
+                    this.passwords_are_equal = true;
+                }
             },
             updateEmail(event) {
                 this.input.email = event.target.value;
                 this.incorrect = false;
-                if (!(this.input.username != "" && this.input.password != "" && this.input.email != "")) {
+                if (!(this.input.username != "" && this.input.password != "" &&
+                                                    this.input.confirm_password && this.input.email != "")) {
                     this.empty = true;
                 }
                 else {
